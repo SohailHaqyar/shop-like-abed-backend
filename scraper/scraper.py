ea1ea0a5ae78f977c7e15b5c836db684a99ec854
Sohail Haqyar <haqyarsohail@gmail.com>
2021-08-01 22:00:09 +0300

    Updated scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,54 +1,39 @@
 import requests
-import json
 from bs4 import BeautifulSoup
 
 
-page1_data = requests.get(
-    'https://www.market-in.gr/el-gr/ALL/eidikh-prosfora?pageno=1')
-
-page2_data = requests.get(
-    'https://www.market-in.gr/el-gr/ALL/eidikh-prosfora?pageno=2'
-)
-page1 = BeautifulSoup(page1_data.text, 'html.parser')
-page2 = BeautifulSoup(page2_data.text, 'html.parser')
-products = []
-for product in page1.find_all('div', {'class': 'product-col'}):
-    a = product.find('a', {'class': 'product-thumb'})
-    link = a.get('href')
-    image = a.find('img').get('src')
-    title = product.find('a', {'class': 'product-ttl'}).text
-    old_price = product.find('span', {'class': 'old-price'}).text
-    new_price = product.find('span', {'class': 'new-price'}).text
-    discount_percentage = product.find('span', {'class': "disc-value"}).text
-    products.append({
-        "link": link,
-        "image_url": image,
-        "title": title.strip(),
-        "old_price": old_price,
-        "new_price": new_price,
-        "discount_details": discount_percentage
-    })
-
-for product in page2.find_all('div', {'class': 'product-col'}):
-    a = product.find('a', {'class': 'product-thumb'})
-    old_price = product.find('span', {'class': 'old-price'})
-    if old_price != None:
-        link = a.get('href')
-        image = a.find('img').get('src')
-        title = product.find('a', {'class': 'product-ttl'}).text
-        new_price = product.find('span', {'class': 'new-price'}).text
-        discount_percentage = product.find(
-            'span', {'class': "disc-value"}).text
-        products.append({
-            "link": link,
-            "image_url": image,
-            "title": title.strip(),
-            "old_price": old_price.text,
-            "new_price": new_price,
-            "discount_details": discount_percentage
-        })
+final_products = []
 
 
-for offer in products:
-    data = requests.post('http://localhost:3000/deals', data=offer)
-    print(data.json())
+def get_data(pageNumber):
+    products = []
+    url = "https://www.market-in.gr/el-gr/ALL/eidikh-prosfora?pageno=" + \
+        str(pageNumber)
+    data = requests.get(url)
+    page = BeautifulSoup(data.text, 'html.parser')
+    for product in page.find_all('div', {'class': 'product-col'}):
+        a = product.find('a', {'class': 'product-thumb'})
+        old_price = product.find('span', {'class': 'old-price'})
+        if old_price != None:
+            link = a.get('href')
+            image = a.find('img').get('src')
+            title = product.find('a', {'class': 'product-ttl'}).text
+            new_price = product.find('span', {'class': 'new-price'}).text
+            discount_percentage = product.find(
+                'span', {'class': "disc-value"}).text
+            products.append({
+                "link": link,
+                "image_url": image,
+                "title": title.strip(),
+                "old_price": old_price.text,
+                "new_price": new_price,
+                "discount_details": discount_percentage
+            })
+    final_products.extend(products)
+    if len(products) != 0:
+        get_data(pageNumber+1)
+
+
+get_data(1)
+print(len(final_products))
+print(final_products)
